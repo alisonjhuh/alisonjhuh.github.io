3ebbdce0191078cc6775ef24b026e7681231ffb3
Ali Huh <ali.j.huh@berkeley.edu>
2020-06-11 03:37:50 -0700

    bootstrap 4 update 6.11.20

---
+++ js/js.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+	// typed
+	var aboutTyped = new Typed('#iam-carousel', {
+		strings: ["i am alison huh", "i am an undergrad", "i am an english major", "i am a digital marketer", "i am a playlist curator", "i am a numtot!"],
+		typeSpeed: 50,
+		backSpeed: 20,
+		smartBackspace: true,
+		backDelay: 1500,
+		fadeOut: false,
+		loop: true,
+		loopCount: Infinity,
+		showCursor: false,
+	});
+});
