c16e2a1b6575ad7da1380d0b8225e7dad78b5d7f
Ali Huh <ali.j.huh@berkeley.edu>
2020-06-11 13:45:41 -0700

    mobile updates 6.11.13.45

--- js/js.js
+++ js/js.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
 	// typed
 	var aboutTyped = new Typed('#iam-carousel', {
-		strings: ["i am alison huh", "i am an undergrad", "i am an english major", "i am a digital marketer", "i am a playlist curator", "i am a numtot!"],
+		strings: ["i am alison huh", "i am a uc berkeley student", "i am an english major", "i am a digital marketer", "i am a playlist curator", "i am a numtot!"],
 		typeSpeed: 50,
 		backSpeed: 20,
 		smartBackspace: true,
