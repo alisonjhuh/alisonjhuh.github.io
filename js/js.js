91597f8b743a032e968a02b1989ce604edd7e4fe
Alison Huh <ali.j.huh@berkeley.edu>
2020-07-28 03:36:08 -0700

    archived about page, migrated to home
    merged about & index,
    restructuring of home elements,
    scroll arrow

--- js/js.js
+++ js/js.js
@@ -1,8 +1,11 @@
 $(document).ready(function() {
 	// typed
 	var aboutTyped = new Typed('#iam-carousel', {
-		strings: ["i am alison huh", "i am a uc berkeley student", "i am an english major", "i am a digital marketer", "i am a playlist curator", "i am a numtot!"],
-		typeSpeed: 50,
+		strings: ["hi there!", "i'm alison huh (-:",
+		"i'm a junior @ uc berkeley", "i'm an english major",
+		"i'm a comp sci minor, too!", "i work as a digital marketer",
+		"i love making playlists", "and i prefer public transit!"],
+		typeSpeed: 60,
 		backSpeed: 20,
 		smartBackspace: true,
 		backDelay: 1500,
