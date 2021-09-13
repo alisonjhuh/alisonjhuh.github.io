5e606d78c86b861dd07ebd99e9175ae43260f424
Alison Huh <ali.j.huh@berkeley.edu>
2021-09-12 23:09:52 -0700

    2021 redesign

--- js/js.js
+++ js/js.js
@@ -2,41 +2,14 @@
 $(document).ready(function() {
 	// typed
 	var aboutTyped = new Typed('#iam-carousel', {
-		strings: ["hi there!", "i'm alison huh :-)",
-		"i'm from los angeles",
-		"i'm a digital marketer",
-		"i'm a technical writer",
-		"i'm a hackathon director",
-		"i'm a student educator",
-		"i'm a playlist curator",
-		"i'm quarantining!",
-		"i'm doing my best. ☺️"],
-		typeSpeed: 60,
-		backSpeed: 40,
-		smartBackspace: true,
-		backDelay: 1500,
+		strings: ["hi there, i'm ali :-)"],
+		typeSpeed: 120,
+		startDelay: 200,
 		fadeOut: false,
-		loop: true,
-		loopCount: Infinity,
+		loop: false,
 		showCursor: false,
 	});
 });
 
 /* AOS */
 AOS.init({ duration: 1000, once: true });
-
-/* Menu Bar */
-function openNav() {
-  if (x.matches) {
-    document.getElementById("myNav").style.width = "100%";
-  } else {
-    document.getElementById("myNav").style.width = "25%";
-  }
-}
-var x = window.matchMedia("(max-width: 1050px)")
-x.addListener(closeNav);
-
-
-function closeNav() {
-  document.getElementById("myNav").style.width = "0%";
-}
